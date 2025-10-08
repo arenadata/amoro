c883d7e68086f56dd4fb9b6402bef28aab9b6178
Andrei Shitov <sai@arenadata.io>
2025-10-08 10:09:18 +0300

    backport(AMORO-3806): Fix a ArithmeticException when calculating health score

--- amoro-format-iceberg/src/main/java/org/apache/amoro/optimizing/plan/CommonPartitionEvaluator.java
+++ amoro-format-iceberg/src/main/java/org/apache/amoro/optimizing/plan/CommonPartitionEvaluator.java
@@ -397,7 +397,7 @@ public class CommonPartitionEvaluator implements PartitionEvaluator {
     long dataFiles = getFragmentFileCount() + getSegmentFileCount();
     long dataRecords = getFragmentFileRecords() + getSegmentFileRecords();
 
-    double averageDataFileSize = dataFilesSize / dataFiles;
+    double averageDataFileSize = dataFiles > 0 ? (double) dataFilesSize / dataFiles : 0;
     double eqDeleteRatio = getNormalizedRatio(equalityDeleteFileRecords, dataRecords);
     double posDeleteRatio = getNormalizedRatio(posDeleteFileRecords, dataRecords);
 
